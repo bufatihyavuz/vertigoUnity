38f83d5adcc61bbb25be012ce2661be9b3229e1b
fattor <fattor1992@gmail.com>
2021-10-17 18:01:56 +0300

    added FirstLesson

---
+++ Assets/Scripts/FirstLessonScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LessonBasic
+{
+    
+}
+
+public class FİrstLesson : MonoBehaviour
+{
+    pyblic int tamSayi = 1;
+    [SerializeField] int tamSayi2 = 100;
+    float ondalikSayi = 0.25f;
+    float ondalikSayi2 = 5.2342f;
+    bool dogru = true;
+    bool yanlis = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
